8990badd0f6f9cfc5cd850db79d0a42d3af9e62a
Michael Okpala <maokpala@gmail.com>
2017-04-07 16:32:04 -0500

    Added string substitution and input preservation

--- main.py
+++ main.py
@@ -1,46 +1,129 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import cgi
+import re
 
-def build_page(textarea_content):
-	username = "<label>Username</label>"
-	username_input = "<input type='text' name='username'/>"
-    
-	password = "<label>Password</label>"
-	password_input = "<input type='text' name='password'/>"
-    
-	verify = "<label>Verify Password</label>"
-	verify_input = "<input type='text' name='verify'/>"
-    
-	email = "<label>Email (optional)</label>"
-	email_input = "<input type='text' name='email'/>"
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Signup</title>
+</head>
+<body>
+    <h2>Signup</h2>
+"""
 
-	submit = "<input type='submit' value='Submit Query'/>"
-	form = "<form method='post'>" + username + "<br>"  + username_input + "<br>" + password + "<br>"  + password_input + "<br>" + verify + "<br>"  + verify_input + "<br>" + email + "<br>"  + email_input + "<br>" + submit + "</form>"
+page_footer = """
+</body>
+</html>
+"""
 
-	header = "<h2>Signup</h2>"
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
     
-	return header + form    
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return USER_RE.match(password)
     
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+def valid_email(email):
+    return EMAIL_RE.match(email)
+
+page_form = """
+<form action='/welcome' method='post'>
+    <table>
+        <tr>
+            <td><label>Username</label></td>
+            <td>
+            <input type='text' name='username' value='%(username)s'/>
+            </td>
+            <td>
+            <div style="color: red">%(error)s</div>
+            </td>
+        </tr>
+        <tr>
+            <td><label>Password</label></td>
+            <td>
+            <input type='password' name='password' value=""/>
+            </td>
+            <td>
+            <div style="color: red">%(error)s</div>
+            </td>
+        </tr>
+        <tr>
+            <td><label>Verify Password</label></td>
+            <td>
+            <input type='password' name='verify' value=""/>
+            </td>
+            <td>
+            <div style="color: red">%(error)s</div>
+            </td>
+        </tr>
+        <tr>
+            <td><label>Email (optional)</label></td>
+            <td>
+            <input type='text' name='email' value="%(email)s"/>
+            </td>
+            <td>
+            <div style="color: red">%(error)s</div>
+            </td>
+        </tr>
+    </table>
+    <input type='submit' value='Submit'/>
+</form>
+"""
+
 class MainHandler(webapp2.RequestHandler):
+
+    def write_form(self, username="", error="", email=""):
+        self.response.write(page_form % {"username": username, "error": error, "email": email})
+    
     def get(self):
-        content = build_page("")
+        self.write_form()
+    
+    def post(self):
+        user_username = self.request.get("username")
+        user_password = self.request.get("password")
+        user_verify = self.request.get("verify")
+        user_email = self.request.get("email")
+        
+        username = valid_username(user_username)
+        password = valid_password(user_password)
+        verify = valid_password(user_verify)
+        email = valid_email(user_email)
+        
+        if username == "":
+            self.write_form("Username cannot be blank.", user_username)
+        if not valid_username(username):
+            self.write_form("Please enter a valid username.", user_username)
+            
+        if password == "":
+            self.write_form("Password cannot be blank.", user_password)
+        if not valid_password(password):
+            self.write_form("Please enter a valid password.", user_password)
+            
+        if verify == "":
+            self.write_form("Please verify your password.", user_verify)
+        if verify != password:
+            self.write_form("Your passwords do not match.", user_password, user_verify)
+            
+        if not valid_email(email):
+            self.write_form("Please enter a valid email address.", user_email)
+            
+        else:
+            self.redirect("/welcome")
+        
+        content = page_header + page_form + page_footer
         self.response.write(content)
 
+class Welcome(webapp2.RequestHandler):        
+    def get(self):
+        username = valid_username(self.request.get("username"))
+        if valid_username(username):
+            welcome_message = "<h2>Welcome, " + username + "</h2>"
+        self.response.write(welcome_message)
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome', Welcome)
 ], debug=True)
